e458fe3e33bb34d79b0396df4480c8d1a8d5db17
Ferry Ayunda <135429317+kisahklasik@users.noreply.github.com>
2024-08-08 23:52:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -45,4 +45,31 @@ createEl.remove();
 document.getElementById("share").innerHTML =
 '<button id="shared">Share</button>';
 
+alert("share image");
+const dataUrl = canvas.toDataURL(); 
+fetch(dataUrl)
+.then(res => res.blob())
+.then(blob => {
+//console.log(blob)
+const filesArray = [new File([blob], 'image.png', { type: blob.type, lastModified: new Date().getTime() })];
+console.log(filesArray);
+const shareData = {
+title: "FREE AND SIMPLE PFP GENERATOR",
+text: "FREE AND SIMPLE PFP GENERATOR",
+url: "https://jiinbe.github.io/fabricjs2/",
+files: filesArray
+};
+console.log(shareData);
+if (navigator.share) {
+navigator.share(
+shareData
+)
+.then(() => alert("thanks for share")) 
+.catch(error => alert("error", error)); 
+} else {
+alert('navigator.share not available'); 
+}
+})
+});
+
 });
